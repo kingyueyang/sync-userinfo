a6e5d3e67031551b49831a11be91d493db457b82
YangYue <kingyueyang@gmail.com>
2012-05-09 14:54:28 +0800

    added: add true loop in check scripts

--- scripts/check_process.sh
+++ scripts/check_process.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-ps aux |grep -v "grep" | grep -v "\.c" |grep "sync-userinfo" > /dev/null
+while true ;
+do
+  ps aux |grep -v "grep" | grep -v "vim" | grep -v "\.c" |grep "sync-userinfo" > /dev/null
 
-if [ $? -eq 0 ]
-then
-  
-  echo sync-userinfo is running!
-else
-  `dirname $0`/../src/sync-userinfo
-  echo sync-userinfo restart ok !
-fi 
+  if [ $? -eq 0 ]
+  then
+    echo sync-userinfo is running!
+  else
+    `dirname $0`/../src/sync-userinfo
+    echo sync-userinfo restart ok !
+  fi 
+done
